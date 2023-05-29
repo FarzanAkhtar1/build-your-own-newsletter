9188dba9c6c4214b4f36fc04c9e4632c97be63fa
FarzanAkhtar1 <farzan.akhtar1@gmail.com>
2023-05-29 22:43:34 +0100

    testing data pulling in pulljson, will merge into main eventually

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
+require('dotenv').config();
+
+console.log(process.env.CONVERTKIT_SECRET);
 console.log("Hello world");
 
-const http = require('https');
-// const req = http.request('https://www.newsnow.co.uk/h/Business+&+Finance/Cryptocurrencies/DeFi', res => {
-// 	const data = [];
-const req = http.request('https://c.newsnow.co.uk/A/1181750939?-52845:39960', res => {
-	const data = [];
-	res.on('data', _ => data.push(_))
-	res.on('end', () => console.log(data.join()))
-});
+function pullNewsData(){
+	const http = require('https');
+	// const req = http.request('https://www.newsnow.co.uk/h/Business+&+Finance/Cryptocurrencies/DeFi', res => {
+	// 	const data = [];
+	// const req = http.request('https://c.newsnow.co.uk/A/1181750939?-52845:39960', res => {
+	// 	const data = [];
+	// 	res.on('data', _ => data.push(_))
+	// 	res.on('end', () => console.log(data.join()))
+	// });
 
-req.end();
+}
+pullNewsData()
+
+function pullSubscribedUsers(){
+	
+	const callURL = "https://api.convertkit.com/v3/subscribers?api_secret="+process.env.CONVERTKIT_SECRET
+	console.log(callURL);
+	//const req = http.request
+
+}
+
+//pullSubscribedUsers()
 
