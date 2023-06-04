83a204e439a3ea537c4f94ec93be853e3ac36edd
FarzanAkhtar1 <farzan.akhtar1@gmail.com>
2023-06-04 16:54:10 +0100

    regex for parsing web page

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 require('dotenv').config();
 const fetch = require('node-fetch');
-
+const fs = require('fs');
 
 //console.log(process.env.CONVERTKIT_SECRET);
 console.log("Hello world");
 
 async function pullNewsData(){
+	fetch(process.env.TEST_SITE).then(function (response) {
+		// The API call was successful!
+		return response.text();
+	}).then(function (html) {
+		// This is the HTML from our response as a text string
+		html = html.replace(/<form class="njs" action="\/h\/ajax\/fredir" method="GET">.*/g,"")
+		html = html.split(/view more headlines.*/g)[0]
+		html = html.replace("Earlier today","")	
+		//html = html.replace(/.*Most Read/gm,"")	
 
-	const resp = await fetch('');
+		var newhtml = html.match(/<a class="hll".*\/a>/g)
+		console.log(newhtml)
+		
+		fs.writeFile('testing.txt', html, err => {
+			if (err) {
+			  console.error(err);
+			}
+			// file written successfully
+		  });
 
-	console.log(await resp);	
+		  fs.writeFile('testing.html', html, err => {
+			if (err) {
+			  console.error(err);
+			}
+			// file written successfully
+		  });
 
+		  return newhtml
+	}).catch(function (err) {
+		// There was an error
+		console.warn('Something went wrong.', err);
+	});
 }	
 
-
-
-
 async function main(){
-	await pullNewsData()
-
+	sites = await pullNewsData()
 }
 //main()
 //getUnsubDataFromGoogleSheets()
