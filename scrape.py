9f66bac119584b79de7c4a5f2ba7830463b4ead4
FarzanAkhtar1 <farzan.akhtar1@gmail.com>
2023-06-04 12:56:04 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,16 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-f = open("scrape.txt", "a")
+f = open("scrape.txt", "w")
+g = open("scrape.html", "w")
+
 URL = os.getenv('TEST_SITE')
 page = requests.get(URL)
 print(page.text)
 f.write(page.text)
 f.close()
+g.write(page.text)
+g.close()
 
-f = open("scrapeRedirect.txt", "a")
+
+f = open("scrapeRedirect.txt", "w")
+g = open("scrapeRedirect.txt", "w")
 URL = os.getenv('REDIRECT')
 page = requests.get(URL)
 print(page.text)
 f.write(page.text)
 f.close()
+g.write(page.text)
+g.close()
