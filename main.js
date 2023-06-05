92102a93c7fed67ae3a89c64d19210a41d4149d2
FarzanAkhtar1 <farzan.akhtar1@gmail.com>
2023-06-05 21:10:14 +0100

    Update main.js
    Code for parsing html from data source.

--- main.js
+++ main.js
@@ -16,24 +16,61 @@ async function pullNewsData(){
 		html = html.replace("Earlier today","")	
 		//html = html.replace(/.*Most Read/gm,"")	
 
-		var newhtml = html.match(/<a class="hll".*\/a>/g)
-		console.log(newhtml)
-		
-		fs.writeFile('testing.txt', html, err => {
-			if (err) {
-			  console.error(err);
-			}
-			// file written successfully
-		  });
+		var newhtml = html.match(/<a class="hll".*<i class.*\d{2}:\d{2}/g)
+		//console.log(newhtml)
+		urlAndHeadline = []
 
-		  fs.writeFile('testing.html', html, err => {
-			if (err) {
-			  console.error(err);
-			}
-			// file written successfully
-		  });
+		//console.log(newhtml)
+		for (x in newhtml){
+			item = newhtml[x]
 
-		  return newhtml
+			date = item.substr(-5)
+
+			item = item.replace("<a class=","")
+			item = item.replace('"hll" href="',"")
+			item = item.replace("</a>","")
+
+			publisher = item.replace(/.*data-pub="/,"")
+			publisher = publisher.replace(/">.*/,"")
+
+			url = item.replace(/" target.*/,"")
+			
+			headline = item.replace(/.*"nofollow">/,"")
+			headline = headline.replace(/<span.*/,"")
+
+			newsData = [publisher, date, url, headline] 
+			//console.log(publisher, date, headline, url)
+
+			if ((newsData[3].toUpperCase()).includes("PRICE") ||
+				(newsData[3].toUpperCase()).includes("BULL") || 
+				(newsData[3].toUpperCase()).includes("BEAR") ||
+				(newsData[3].toUpperCase()).includes("RALL") || //RALLY or RALLIES				
+				(newsData[3].toUpperCase()).includes("TRAD") ||
+				(newsData[3].toUpperCase()).includes("SUPPORT") ||
+				(newsData[3].toUpperCase()).includes("$") ||
+				(newsData[3].toUpperCase()).includes("SUPPORT") ||
+				(newsData[3].toUpperCase()).includes("RESISTANCE") ||
+				(newsData[3].toUpperCase()).includes("ARTIFICIAL") ||
+				(newsData[3].toUpperCase()).includes("PUMP") ||
+				(newsData[3].toUpperCase()).includes("DUMP") ||
+				(newsData[3].toUpperCase()).includes("INSIDE BITCOIN")){
+				console.log("detected", newsData[3])
+			}else{
+				urlAndHeadline.push(newsData)
+
+			}
+		}
+		console.log(urlAndHeadline.length)	
+
+		// fs.writeFile('sorted.txt', String(urlAndHeadline), err => {
+		// 	if (err) {
+		// 	  console.error(err);
+		// 	}
+		// 	// file written successfully
+		//   });
+
+
+	
 	}).catch(function (err) {
 		// There was an error
 		console.warn('Something went wrong.', err);
@@ -42,6 +79,7 @@ async function pullNewsData(){
 
 async function main(){
 	sites = await pullNewsData()
+	//console.log(sites)
 }
 //main()
 //getUnsubDataFromGoogleSheets()
