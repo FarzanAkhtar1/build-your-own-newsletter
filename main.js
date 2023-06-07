c2e45c6b8fccba6bfa054c5777b9af7fe3a7ce11
FarzanAkhtar1 <farzan.akhtar1@gmail.com>
2023-06-07 22:26:57 +0100

    More article parsing

--- main.js
+++ main.js
@@ -5,85 +5,86 @@ const fs = require('fs');
 //console.log(process.env.CONVERTKIT_SECRET);
 console.log("Hello world");
 
-async function pullNewsData(){
-	fetch(process.env.TEST_SITE).then(function (response) {
-		// The API call was successful!
-		return response.text();
-	}).then(function (html) {
-		// This is the HTML from our response as a text string
-		html = html.replace(/<form class="njs" action="\/h\/ajax\/fredir" method="GET">.*/g,"")
-		html = html.split(/view more headlines.*/g)[0]
-		html = html.replace("Earlier today","")	
-		//html = html.replace(/.*Most Read/gm,"")	
+async function pullNewsData(coins, tickers){
+	for (y in coins){
+		await fetch(process.env.TEST_SITE + coins[y] + "?type=ln").then(function (response) {
+			return response.text();
+		}).then(function (html) {
+			// This is the HTML from our response as a text string
+			html = html.replace(/<form class="njs" action="\/h\/ajax\/fredir" method="GET">.*/g,"")
+			html = html.split(/view more headlines.*/g)[0]
+			html = html.replace("Earlier today","")	
+			//html = html.replace(/.*Most Read/gm,"")	
 
-		var newhtml = html.match(/<a class="hll".*<i class.*\d{2}:\d{2}/g)
-		//console.log(newhtml)
-		urlAndHeadline = []
+			//////////////////////////NEED NEW REGEX TO CONSIDER THE DATE IN THE FORMAT HH:MM DD-MMM-YYYY
+			var newhtml = html.match(/<a class="hll".*<i class.*\d{2}:\d{2}/g)
+			//console.log(newhtml)
+			urlAndHeadline = []
 
-		//console.log(newhtml)
-		for (x in newhtml){
-			item = newhtml[x]
+			//console.log(newhtml)
+			for (x in newhtml){
+				item = newhtml[x]
 
-			date = item.substr(-5)
+				//////////////////////////NEED NEW REGEX TO CONSIDER THE DATE IN THE FORMAT HH:MM DD-MMM-YYYY
+				date = item.substr(-5)
 
-			item = item.replace("<a class=","")
-			item = item.replace('"hll" href="',"")
-			item = item.replace("</a>","")
+				item = item.replace("<a class=","")
+				item = item.replace('"hll" href="',"")
+				item = item.replace("</a>","")
 
-			publisher = item.replace(/.*data-pub="/,"")
-			publisher = publisher.replace(/">.*/,"")
+				publisher = item.replace(/.*data-pub="/,"")
+				publisher = publisher.replace(/">.*/,"")
 
-			url = item.replace(/" target.*/,"")
-			
-			headline = item.replace(/.*"nofollow">/,"")
-			headline = headline.replace(/<span.*/,"")
+				url = item.replace(/" target.*/,"")
+				
+				headline = item.replace(/.*"nofollow">/,"")
+				headline = headline.replace(/<span.*/,"")
 
-			newsData = [publisher, date, url, headline] 
-			//console.log(publisher, date, headline, url)
+				newsData = [publisher, date, url, headline] 
+				//console.log(publisher, date, headline, url)
 
-			// Add iuf it doesn't contain the name of the actual asset
-			if (((newsData[3].toUpperCase()).includes("PRICE") ||
-				(newsData[3].toUpperCase()).includes("BULL") || 
-				(newsData[3].toUpperCase()).includes("BEAR") ||
-				(newsData[3].toUpperCase()).includes("RALL") || //RALLY or RALLIES				
-				(newsData[3].toUpperCase()).includes("TRAD") ||
-				(newsData[3].toUpperCase()).includes("SUPPORT") ||
-				(newsData[3].toUpperCase()).includes("$") ||
-				(newsData[3].toUpperCase()).includes("SUPPORT") ||
-				(newsData[3].toUpperCase()).includes("RESISTANCE") ||
-				(newsData[3].toUpperCase()).includes("ARTIFICIAL") ||
-				(newsData[3].toUpperCase()).includes("PUMP") ||
-				(newsData[3].toUpperCase()).includes("DUMP") ||
-				(newsData[3].toUpperCase()).includes("INSIDE BITCOIN")
-				|| //other ore function
-				((newsData[3].toUpperCase()).includes("INSIDE BITCOIN") ||
-				(newsData[3].toUpperCase()).includes("INSIDE BITCOIN") ||
-				(newsData[3].toUpperCase()).includes("INSIDE BITCOIN") ||
-				)
-				)){
-				console.log("detected", newsData[3])
-			}else{
-				urlAndHeadline.push(newsData)
-
+				// Add iuf it doesn't contain the name of the actual asset
+				if ((newsData[3].toUpperCase()).includes("PRICE") ||
+					(newsData[3].toUpperCase()).includes("BULL") || 
+					(newsData[3].toUpperCase()).includes("BEAR") ||
+					(newsData[3].toUpperCase()).includes("RALL") || //RALLY or RALLIES				
+					(newsData[3].toUpperCase()).includes("TRAD") ||
+					(newsData[3].toUpperCase()).includes("SUPPORT") ||
+					(newsData[3].toUpperCase()).includes("$") ||
+					(newsData[3].toUpperCase()).includes("SUPPORT") ||
+					(newsData[3].toUpperCase()).includes("RESISTANCE") ||
+					(newsData[3].toUpperCase()).includes("ARTIFICIAL") ||
+					(newsData[3].toUpperCase()).includes("PUMP") ||
+					(newsData[3].toUpperCase()).includes("DUMP") ||
+					(newsData[3].toUpperCase()).includes("INSIDE BITCOIN") ||
+					(newsData[3].toUpperCase()).includes("TECHNICAL") ||
+					!((/\d{2}:\d{2}/g).test(newsData[2]))
+					// !(newsData[3].includes(coins[y])) ||
+					// !(newsData[3].includes(tickers[y]))
+					){
+					console.log("Dirty -", newsData[3], newsData[0], newsData[1])
+				}else{
+					urlAndHeadline.push(newsData)
+					console.log("Clean -", newsData[3], newsData[0], newsData[1])
+				}
 			}
-		}
-		console.log(urlAndHeadline.length)	
-		console.log(urlAndHeadline)
+			console.log(tickers[y], coins[y], urlAndHeadline.length)
 
-		// fs.writeFile('sorted.txt', String(urlAndHeadline), err => {
-		// 	if (err) {
-		// 	  console.error(err);
-		// 	}
-		// 	// file written successfully
-		//   });
+			// fs.writeFile('sorted.txt', String(urlAndHeadline), err => {
+			// 	if (err) {
+			// 	  console.error(err);
+			// 	}
+			// 	// file written successfully
+			//   });
 
 
-	
-	}).catch(function (err) {
-		// There was an error
-		console.warn('Something went wrong.', err);
-	});
-}	
+		
+		}).catch(function (err) {
+			// There was an error
+			console.warn('Something went wrong.', err);
+		});
+	}	
+}
 
 async function sendEmail(newsOfEachCoin){
 	//newsOfEachCoin should be a dictionary, where the key is the name of the crypto, and the value is an array of arrays where the array contains the link to the article and the headline
@@ -101,7 +102,9 @@ async function sendEmail(newsOfEachCoin){
 }
 
 async function main(){
-	sites = await pullNewsData()
+	names = ['Bitcoin','Algorand']
+	ticker = ['BTC', 'ALGO']
+	sites = await pullNewsData(names, ticker)
 	//console.log(sites)
 }
 //main()
